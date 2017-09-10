489f0f127759157c746c3b319b778007b3b32808
Trevor Meadows <tlm04070@gmail.com>
2017-09-10 15:48:39 -0400

    added styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+var option1 = {
+    name: "example1",
+    "healthPoints": 400,
+    "attackPower": 50,
+    "counterAttackPower": 20,
+}
+var option2 = {
+    name: "example2",
+    "healthPoints": 275,
+    "attackPower": 75,
+    "counterAttackPower": 25,
+}
+var option3 = {
+    name: "example3",
+    "healthPoints": 550,
+    "attackPower": 35,
+    "counterAttackPower": 15,
+}
+var option4 = {
+    name: "example4",
+    "healthPoints": 600,
+    "attackPower": 30,
+    "counterAttackPower": 10,
+}
+
