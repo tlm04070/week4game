f42beba7a17784dd767a011586085dfec2ce00b2
Trevor Meadows <tlm04070@gmail.com>
2017-09-10 23:10:36 -0400

    added values to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,84 @@
-var option1 = {
-    name: "example1",
-    "healthPoints": 400,
-    "attackPower": 50,
-    "counterAttackPower": 20,
+
+
+
+$("#blackMage") == blackmage;
+var blackmage = {
+    name: "blackmage",
+    healthPoints: 400,
+    attackPower: 50,
+    counterAttackPower: 20, 
 }
-var option2 = {
-    name: "example2",
-    "healthPoints": 275,
-    "attackPower": 75,
-    "counterAttackPower": 25,
+
+
+
+$("#goomba") == goomba;
+var goomba = {
+    name: "goomba",
+    healthPoints: 275,
+    attackPower: 75,
+    counterAttackPower: 25,
 }
-var option3 = {
-    name: "example3",
-    "healthPoints": 550,
-    "attackPower": 35,
-    "counterAttackPower": 15,
+
+
+
+
+$("#aginah") == aginah;
+var aginah = {
+    name: "aginah",
+    healthPoints: 550,
+    attackPower: 35,
+    counterAttackPower: 15,
 }
-var option4 = {
-    name: "example4",
-    "healthPoints": 600,
-    "attackPower": 30,
-    "counterAttackPower": 10,
+
+
+
+
+
+$("#invaders") == invaders;
+var invaders = {
+    name: "invaders",
+    healthPoints: 600,
+    attackPower: 30,
+    counterAttackPower: 10,
 }
+
+
+
+
+console.log("testing link");
+
+console.log($("#goomba"));
+console.log($("#aginah"));
+console.log($("#invaders"));
+
+$("#bmName").append(blackmage.name);
+$("#bmHealthPoints").append(blackmage.healthPoints);
+$("#bmAttackPower").append(blackmage.attackPower);
+$("#bmCounterAttackPower").append(blackmage.counterAttackPower);
+
+$("#goombaName").append(goomba.name);
+$("#goombaHealthPoints").append(goomba.healthPoints);
+$("#goombaAttackPower").append(goomba.attackPower);
+$("#goombaCounterAttackPower").append(goomba.counterAttackPower);
+
+$("#aginahName").append(aginah.name);
+$("#aginahHealthPoints").append(aginah.healthPoints);
+$("#aginahAttackPower").append(aginah.attackPower);
+$("#aginahCounterAttackPower").append(aginah.counterAttackPower);
+
+$("#invadersName").append(invaders.name);
+$("#invadersHealthPoints").append(invaders.healthPoints);
+$("#invadersAttackPower").append(invaders.attackPower);
+$("#invadersCounterAttackPower").append(invaders.counterAttackPower);
+
+
+
+
+
+
+
+
+
+
+
 
